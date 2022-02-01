b507f26ecb38cbdad96664b9ecbb056392e0fe4a
sean <43609023+spnda@users.noreply.github.com>
2022-02-01 19:06:30 +0100

    Change: Use carbon from submodule

--- src/carbon/resource/stagingbuffer.cpp
+++
@@ -1,8 +0,0 @@
-#include <carbon/resource/stagingbuffer.hpp>
-
-carbon::StagingBuffer::StagingBuffer(std::shared_ptr<carbon::Device> device, VmaAllocator allocator, std::string name)
-    : Buffer(std::move(device), allocator, std::move(name)) {}
-
-void carbon::StagingBuffer::create(uint64_t bufferSize, VkBufferUsageFlags additionalBufferUsage) {
-    Buffer::create(bufferSize, bufferUsage | additionalBufferUsage, memoryUsage, memoryProperties);
-}
